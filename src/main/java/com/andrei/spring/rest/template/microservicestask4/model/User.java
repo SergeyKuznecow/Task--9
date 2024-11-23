6edaa126b55bb46a97a38d3ee314ec36e6a59fe3
SergeyKuznecow <gfsn@rambler.ru>
2024-11-24 01:57:50 +0300

    Initial commit

---
+++ src/main/java/com/andrei/spring/rest/template/microservicestask4/model/User.java
@@ -0,0 +1,4 @@
+package com.andrei.spring.rest.template.microservicestask4.model;
+
+public record User(Long id, String name, String lastName,Byte age) {
+}
---
+++ src/test/java/com/andrei/spring/rest/template/microservicestask4/MicroservicesTask4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.spring.rest.template.microservicestask4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesTask4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
